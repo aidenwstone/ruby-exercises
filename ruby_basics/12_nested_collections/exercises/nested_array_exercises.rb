e306a9d2026161f6adafe9c12b2e6d6714d9a453
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2025-06-09 15:16:08 -0400

    Implement find_favorite method

--- ruby_basics/12_nested_collections/exercises/nested_array_exercises.rb
+++ ruby_basics/12_nested_collections/exercises/nested_array_exercises.rb
@@ -1,3 +1,5 @@
+require 'pry-byebug'
+
 def blank_seating_chart(number_of_rows, seats_per_row)
   # return a 2d array to represent a seating chart that contains
   # number_of_rows nested arrays, each with seats_per_row entries of nil to
@@ -42,14 +44,5 @@ def find_favorite(array_of_hash_objects)
   # take an array_of_hash_objects and return the hash which has the key/value
   # pair :is_my_favorite? => true. If no hash returns the value true to the key
   # :is_my_favorite? it should return nil
-
-  # array_of_hash_objects will look something like this:
-  # [
-  #   { name: 'Ruby', is_my_favorite?: true },
-  #   { name: 'JavaScript', is_my_favorite?: false },
-  #   { name: 'HTML', is_my_favorite?: false }
-  # ]
-
-  # TIP: there will only be a maximum of one hash in the array that will
-  # return true to the :is_my_favorite? key
+  array_of_hash_objects.find { |hash| hash[:is_my_favorite?] }
 end
